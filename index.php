9cb90463085c5e7e21ac3dab3b99ce716a4e0829
jeniatr <JeniaTr@yandex.ru>
2016-10-23 23:51:20 +0300

    Приминение библиотеки ростояния между кординатами

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jenia
- * Date: 23.10.16
- * Time: 21:57
- */
+require 'vendor/autoload.php';
+
+use Location\Coordinate;
+use Location\Distance\Vincenty;
+
+$coord1 = new Coordinate(49.378518, 32.152849);
+$coord2 = new Coordinate(49.426007, 32.094617);
+
+$calculator = new Vincenty();
+
+echo 'Дом----Geekhub';
+echo '<br>';
+echo '<br>';
+echo 'Ростояние между двумя кординатами :  ', $calculator->getDistance($coord1, $coord2), ' метров';
+
+
+?>
